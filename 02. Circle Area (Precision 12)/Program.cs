177df255ba8729709b016b76f4e7c3dda334fc36
KrisoKolev <kristian.kolevv@gmail.com>
2017-01-18 22:26:42 +0200

    The first homework.

---
+++ 02. Circle Area (Precision 12)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+        double pi = Math.PI;
+        Console.WriteLine("{0:f12}", pi * r * r);
+    }
+}
+
---
+++ 05. Special Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            int sumOfDigits = 0;
+            int num = i;
+
+            while (num > 0)
+            {
+                sumOfDigits = num % 10;
+                num /= 10;
+            }
+            bool isSpecial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+            Console.WriteLine($"{i} -> {isSpecial}");
+        }
+    }
+}
+
