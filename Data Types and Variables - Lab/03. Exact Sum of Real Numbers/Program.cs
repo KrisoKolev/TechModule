ff43bca840d434478fd2664c123ad5ab6f811e9a
KrisoKolev <kristian.kolevv@gmail.com>
2017-01-18 22:34:08 +0200

    Update

---
+++ Data Types and Variables - Lab/03. Exact Sum of Real Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        decimal n = decimal.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            decimal numbers = decimal.Parse(Console.ReadLine());
+            sum = sum + numbers;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
---
+++ Data Types and Variables - Lab/04. Elevator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int waitingPeople = int.Parse(Console.ReadLine());
+        int elevatorCapacity = int.Parse(Console.ReadLine());
+        int courses = (int)Math.Ceiling((double)waitingPeople / elevatorCapacity);
+
+        Console.WriteLine(courses);
+    }
+}
+
