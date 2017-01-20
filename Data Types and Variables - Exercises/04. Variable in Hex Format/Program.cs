54dccbc8429f1076296bff081e693dfa4e97663e
KrisoKolev <kristian.kolevv@gmail.com>
2017-01-21 00:34:14 +0200

    Update Data Types and Variables - Exercises "4" and "14"

--- Data Types and Variables - Exercises/04. Variable in Hex Format/Program.cs
+++ Data Types and Variables - Exercises/04. Variable in Hex Format/Program.cs
@@ -5,16 +5,10 @@ class Program
     static void Main()
     {
         string number = Console.ReadLine();
-        string number2 = Console.ReadLine();
-        string number3 = Console.ReadLine();
 
-        int convert = Convert.ToInt32(number);
-        int convert2 = Convert.ToInt32(number2);
-        int convert3 = Convert.ToInt32(number3);
+        decimal convert = Convert.ToInt32(number, 16);
 
         Console.WriteLine(convert);
-        Console.WriteLine(convert2);
-        Console.WriteLine(convert3);
     }
 }
 
--- Data Types and Variables - Exercises/14. Integer to Hex/Program.cs
+++ Data Types and Variables - Exercises/14. Integer to Hex/Program.cs
@@ -3,7 +3,12 @@ class Program
 {
     static void Main()
     {
+        int num = int.Parse(Console.ReadLine());
+        string numHex = num.ToString("X");
+        string binary = Convert.ToString(num, 2);
 
+        Console.WriteLine(numHex);
+        Console.WriteLine(binary);
     }
 }
 
